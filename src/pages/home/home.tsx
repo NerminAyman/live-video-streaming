5492cd8a8f8946ff2a874003c984f77696daccd2
Nermin Ayman <nermin.ayman@almentor.net>
2022-11-16 04:52:58 +0200

    add agora ui kit demo

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,22 +1,24 @@
 import './home.scss';
 import {useState} from "react";
 import {MeetingRoom} from "../../components/meeting-room/meeting-room";
+import UiKitDemo from "../../components/ui-kit-demo";
 
 export function Home() {
     const [inCall, setInCall] = useState(false);
 
     return (
         <div className='home'>
-            {inCall ? (
-                <MeetingRoom setInCall={setInCall}/>
-            ) : (
-                <button
-                    color="primary"
-                    onClick={() => setInCall(true)}
-                >
-                    Join Call
-                </button>
-            )}
+            {/*{inCall ? (*/}
+            {/*    <MeetingRoom setInCall={setInCall}/>*/}
+            {/*) : (*/}
+            {/*    <button*/}
+            {/*        color="primary"*/}
+            {/*        onClick={() => setInCall(true)}*/}
+            {/*    >*/}
+            {/*        Join Call*/}
+            {/*    </button>*/}
+            {/*)}*/}
+            <UiKitDemo/>
         </div>
     );
 }
