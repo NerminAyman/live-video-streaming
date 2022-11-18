019cab9492d0a57f27ca8dc4c69950df8fc7420c
Nermin Ayman <nermin.ayman@almentor.net>
2022-11-18 16:13:32 +0200

    add new token

--- src/services/stream-config.service.ts
+++ src/services/stream-config.service.ts
@@ -1,4 +1,4 @@
 const appId = '89def2fb3dc44b3985e2c766c1fbdfa5';
-const token = '007eJxTYFjHOq9Ah9tur6rmDqXpffoJXW8V9+//Lvdn1hW3FmXl028UGCwsU1LTjNKSjFOSTUySjC0tTFONks3NzJIN05JS0hJNtV6WJjcEMjLMO8HAxMgAgSA+C0NGfm4qAwMAMMofpA==';
+const token = '007eJxTYDBffqfJu9Gbs/Va0l9FU88vl8y2qnUrTJnyUOxq4ZTLXIYKDBaWKalpRmlJxinJJiZJxpYWpqlGyeZmZsmGaUkpaYmmPAHlyQ2BjAzPv9SwMjJAIIjPwpCRn5vKwAAAUuQf9g==';
 
 export const config = {mode: "rtc", codec: "vp8", appId: appId, token: token};
