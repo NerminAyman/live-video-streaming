aeda854c3f4c6979d542ea89d6b2be5dcbb65501
Nermin Ayman <nermin.ayman@almentor.net>
2022-11-19 15:09:54 +0200

    fix merge issues

---
+++ src/services/agora-config.service.ts
@@ -0,0 +1,4 @@
+const appId = '89def2fb3dc44b3985e2c766c1fbdfa5';
+const token = '007eJxTYLB5/E5boO2E56y69TdtVp8+UHNO/TFL47X46xap5758OPZTgcHCMiU1zSgtyTgl2cQkydjSwjTVKNnczCzZMC0pJS3R9MPiiuSGQEYG1/xDDIxQCOKzMGTk56YyMAAAeCUj7g==';
+
+export const config = {mode: "rtc", codec: "vp8", appId: appId, token: token};
