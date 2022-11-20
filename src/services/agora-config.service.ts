93693439c9a63a44374572c97cdfadad06b8fcf5
Nermin Ayman <nermin.ayman@almentor.net>
2022-11-20 18:02:24 +0200

    update the expired token

--- src/services/agora-config.service.ts
+++ src/services/agora-config.service.ts
@@ -1,4 +1,4 @@
 const appId = '89def2fb3dc44b3985e2c766c1fbdfa5';
-const token = '007eJxTYPgn+GhLgYXkXunvE+SPcDBssTUu+brOw01b//Trjy/8ki8oMFhYpqSmGaUlGackm5gkGVtamKYaJZubmSUbpiWlpCWa6ktWJTcEMjJM/LSdlZEBAkF8FoaM/NxUBgYAdpggbQ==';
+const token = '007eJxTYMjfK//K+96VNXZ3LcrKONOeHdF9dFxq3n13rdsGots4a6sVGCwsU1LTjNKSjFOSTUySjC0tTFONks3NzJIN05JS0hJNt/tWJTcEMjJ4lFxkYIRCEJ+FISM/N5WBAQCa/CB/';
 
 export const config = {mode: "rtc", codec: "vp8", appId: appId, token: token};
