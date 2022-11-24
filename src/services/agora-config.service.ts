6b4a60dd314f80c35857751ae437c805205c20f8
Nermin Ayman <nermin.ayman@almentor.net>
2022-11-24 13:59:27 +0200

    update temp token

--- src/services/agora-config.service.ts
+++ src/services/agora-config.service.ts
@@ -1,4 +1,4 @@
 const appId = '89def2fb3dc44b3985e2c766c1fbdfa5';
-const token = '007eJxTYPghdcH4aDXnxDSNz3uXiPGuabk1tdfy+PndCZG+nK1+QSUKDBaWKalpRmlJxinJJiZJxpYWpqlGyeZmZsmGaUkpaYmmem11yQ2BjAx6E+4xMEIhiM/CkJGfm8rAAABbxB+p';
+const token = '007eJxTYGBZJHHkAONl8zcu/5X231wm8EBQolhy/tZQ8aI1ryJEDu1RYLCwTElNM0pLMk5JNjFJMra0ME01SjY3M0s2TEtKSUs0DY6pT24IZGQo+13PyMgAgSA+C0NGfm4qAwMASgYf2w==';
 
 export const config = {mode: "rtc", codec: "vp8", appId: appId, token: token};
